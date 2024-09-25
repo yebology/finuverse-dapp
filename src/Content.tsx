36016f8a3cb6b22f0ad8c64d3dc4b1d2ad7c59dd
Louis Fernando <ferlouis55@gmail.com>
2024-09-25 21:35:39 +0700

    Add state for each input & make it functional ; Make component for question & section in CourseForm page ; Add animated pop up using SweetAlert2 ; Add animated scrolling using AOS

--- src/Content.tsx
+++ src/Content.tsx
@@ -2,7 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./views/Home";
 import { Navbar } from "./components/fixed/Navbar";
 import { Footer } from "./components/fixed/Footer";
-import { CompleteModal } from "./components/modals/CompleteModal";
+import { CourseForm } from "./views/CourseForm";
+// import { CompleteModal } from "./components/modals/CompleteModal";
 
 export const Content = () => {
   return (
@@ -10,6 +11,7 @@ export const Content = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create_course" element={<CourseForm />} />
       </Routes>
       <Footer />
       {/* <CompleteModal /> */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,41 @@
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
+};
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({
+  addBase,
+  theme
+}) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+  addBase({
+    ":root": newVars,
+  });
 }
